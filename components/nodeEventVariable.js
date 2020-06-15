5fc0509e2cca3a138280e4874cc021cc7c0d5570
phillipsnj <nigeljphillips@yahoo.co.uk>
2020-06-15 18:53:34 +0100

    Updates and bug fixes.

---
+++ components/nodeEventVariable.js
@@ -0,0 +1,57 @@
+Vue.component('nodeEventVariable', {
+    name: "nodeEventVariable",
+    props: ["nodeId", "actionId", "varId", "name"],
+    data: () => ({
+        rules: [
+            value => value >= 0 || 'Cannot be a negative number',
+            value => value <= 255 || 'Number to High'
+        ],
+        label: "",
+        variableLocal: 0,
+        eventName: ""
+    }),
+    mounted() {
+        console.log(`nodeEventVariable Mounted : ${this.$store.state.nodes[this.nodeId].actions[this.actionId].variables[this.varId]}`)
+        this.variableLocal = this.$store.state.nodes[this.nodeId].actions[this.actionId].variables[this.varId]
+        this.eventName = this.$store.state.nodes[this.nodeId].actions[this.actionId].event
+        if (this.name) {
+            this.label = this.name
+        } else {
+            this.label = `Event Variable ${this.varId}`
+        }
+    },
+    watch: {
+        variableValue() {
+            this.variableLocal = this.$store.state.nodes[this.nodeId].actions[this.actionId].variables[this.varId]
+        }
+    },
+    computed: {
+        variableValue: function() {
+            return this.$store.state.nodes[this.nodeId].actions[this.actionId].variables[this.varId]
+        }
+    },
+    methods: {
+        updateEV: function () {
+            console.log(`Update Event Variable : ${this.node} : ${this.action}`)
+            this.$root.send('EVLRN', {
+                "nodeId": this.nodeId,
+                "actionId": this.actionId,
+                "eventName": this.eventName,
+                "eventId": this.varId,
+                "eventVal": this.variableLocal
+            })
+        }
+    },
+    template: `
+        <v-card class="xs6 md3 pa-3" flat>
+            <v-text-field
+                    :label="label"
+                    v-model="variableLocal"
+                    outlined
+                    :rules="rules"
+                    @change="updateEV"
+            >
+            </v-text-field>
+        </v-card>`
+})
+
---
+++ components/nodeEventVariables.js
@@ -0,0 +1,37 @@
+Vue.component('nodeEventVariables', {
+    name: "nodeEventVariables",
+    props: ['nodeId','actionId'],
+    mounted() {
+        console.log(`nodeEventVariables mounted : ${this.nodeId} :: ${this.actionId}`)
+        for (let i = 1; i <= this.node.parameters[5]; i++) {
+            this.$root.send('REVAL', {"nodeId": this.nodeId, "actionId": this.actionId, "valueId": i})
+        }
+    },
+    computed: {
+        node: function () {
+            return this.$store.state.nodes[this.nodeId]
+        }
+    }/*,
+    methods: {
+        getEventVariables: function (actionId) {
+            console.log(`getEventVariables(${actionId})`)
+            for (let i = 1; i <= this.node.parameters[5]; i++) {
+                this.$root.send('REVAL', {"nodeId": this.nodeId, "actionId": actionId, "valueId": i})
+            }
+        }
+    }*/,
+    template: `
+        <v-container>
+            <h3>Node Variables</h3>
+            <v-row>
+                <nodeEventVariable v-bind:nodeId="nodeId" 
+                                   v-bind:actionId="actionId"
+                              v-bind:varId="n" 
+                              v-for="n in node.parameters[5]"
+                              :key="n">
+                    
+                </nodeEventVariable>
+            </v-row>
+            <p>{{ node.actions[actionId] }}</p>
+        </v-container>`
+})
